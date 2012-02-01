a5f15aee337aa6192d726c477eb3b25b96e8c213
luolE <cqluole@tom.com>
2012-02-01 16:25:22 +0800

    anything-mode 插件配置更新

--- site-lisp/init-anything.el
+++ site-lisp/init-anything.el
@@ -6,24 +6,40 @@
   '((name . "麻辣江湖文件列表")
     (candidates . (lambda()
 					(append
-					 (get-files-recursive "D:/adin.works/mj_src/gameworld/product/debug/server/gameserver/script/pvp/yongzhe" "\\.lua$")
-					 (get-files-recursive "D:/adin.works/mj_src/gameworld/product/debug/server/gameserver/script/task/daily_task" "\\.lua$")
-					 (get-files-recursive "d:/adin.works/mj_src/gameworld/product/debug/server/gameserver/script/task/counter_strike" "\\.lua$")
-					 (get-files-recursive "d:/adin.works/mj_src/gameworld/product/debug/server/gameserver/script/task/gourmetrestaurant" "\\.lua$")
+					 (get-files-recursive 
+					  "D:/adin.works/mj_src/gameworld/product/debug/server/gameserver/script/pvp/yongzhe" 
+					  "\\.lua$")
+					 (get-files-recursive 
+					  "D:/adin.works/mj_src/gameworld/product/debug/server/gameserver/script/task/daily_task" 
+					  "\\.lua$")
+					 (get-files-recursive 
+					  "d:/adin.works/mj_src/gameworld/product/debug/server/gameserver/script/task/counter_strike" 
+					  "\\.lua$")
+					 (get-files-recursive 
+					  "d:/adin.works/mj_src/gameworld/product/debug/server/gameserver/script/task/gourmetrestaurant" 
+					  "\\.lua$")
+					 (get-files-recursive 
+					  "d:/adin.works/mj_src/gameworld/product/debug/server/gameserver/script/npc" 
+					  "\\.lua$")
 					)
 					)
 )
 	
-    (type . file)
+    ;; (type . file)
+	(action . find-file)
     (requires-pattern . 2)
     (delayed)))
 
 (eval-after-load 'anything
   '(progn
-	 (require 'anything-config)
+	 
+	 ;; 高亮匹配字符
+	 (require 'anything-match-plugin)
+	 
 	 ;; 设置按键
 	 (define-key anything-map (kbd "M-p") 'anything-previous-line)
 	 (define-key anything-map (kbd "M-n") 'anything-next-line)
+	 
 	 ;; 设置搜索源
 	 (setq anything-sources
 		   (list 
