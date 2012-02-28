00b6473018ff1f15258ebb9a14f81d0aeba68d56
luolE <cqluole@tom.com>
2012-02-28 20:49:37 +0800

    flymake-python
    Signed-off-by: luolE <cqluole@tom.com>

--- site-lisp/init-python-mode.el
+++ site-lisp/init-python-mode.el
@@ -7,6 +7,22 @@
      ;; etags interface
      )))
 
+;;--------------------------------------------------------------------
+;; flymake-mode
+;;--------------------------------------------------------------------
+(when (load "flymake" t)
+  (defun flymake-pylint-init ()
+	(let* ((temp-file (flymake-init-create-temp-buffer-copy
+					   'flymake-create-temp-inplace))
+           (local-file (file-relative-name
+                        temp-file
+                        (file-name-directory buffer-file-name))))
+	  (list "pyflakes" (list local-file))))
+  
+  (add-to-list 'flymake-allowed-file-name-masks
+               '("\\.py\\'" flymake-pylint-init)))
+
+(add-hook 'find-file-hook 'flymake-find-file-hook)
 
 ;;--------------------------------------------------------------------
 ;; python-mode 
